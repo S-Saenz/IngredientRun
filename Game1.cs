11c42d52d406f990dd3630166ed434aee3826523
Shui Lau <shhlau@ucsc.edu>
2020-11-10 17:20:33 -0800

    Background + player

--- Game1.cs
+++ Game1.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Windows.Forms.VisualStyles;
 //hi
 //123
 namespace IngredientRun
 {
     public class Game1 : Game
     {
+        Texture2D refugee;
+        Texture2D background;
+
+        Vector2 refugeePos;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -19,8 +24,8 @@ namespace IngredientRun
             _graphics.GraphicsProfile = GraphicsProfile.HiDef;
 
 
-            _graphics.PreferredBackBufferWidth = 1239;//1241;  // set this value to the desired width of your window
-            _graphics.PreferredBackBufferHeight = 800;   // set this value to the desired height of your window
+            _graphics.PreferredBackBufferWidth = 1728;//1241;  // set this value to the desired width of your window
+            _graphics.PreferredBackBufferHeight = 972;   // set this value to the desired height of your window
             _graphics.ApplyChanges();
 
         }
@@ -28,7 +33,8 @@ namespace IngredientRun
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            refugeePos = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+            _graphics.PreferredBackBufferHeight / 2);
             base.Initialize();
         }
 
@@ -37,6 +43,8 @@ namespace IngredientRun
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            background = Content.Load<Texture2D>("bg/Ingredient Run Camp");
+            refugee = Content.Load<Texture2D>("chars/refugee");
         }
 
         protected override void Update(GameTime gameTime)
@@ -54,6 +62,13 @@ namespace IngredientRun
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(background, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0.9f);
+            _spriteBatch.Draw(refugee, refugeePos,null, Color.White, 0f, Vector2.Zero, 0.5f, SpriteEffects.None, 0f);
+         
+            //spriteBatch.Draw(texture, position, null, Color.White, 0f, 
+            //Vector2.Zero, 0.5f, SpriteEffects.None, 0f);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
