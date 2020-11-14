3cf6229fc338ded7951ca66c60a77adc7318a81b
S-Saenz <sarsaenz@ucsc.edu>
2020-11-13 23:06:48 -0800

    Starting Inventory Class

---
+++ Game/Inventory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace IngredientRun
+{
+    class Inventory
+    {
+
+        public Inventory()
+        {
+        }
+
+        public void Load(ContentManager Content)
+        {
+
+        }
+
+        public void Update(MouseState mouseState)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
