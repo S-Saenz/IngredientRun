563ddf8593a421fcbfa1e768e51099e200c6bb1a
coagulantLegume <alvwolf@ucsc.edu>
2021-01-12 18:23:36 -0800

    added waypoint functionality and wall collision layer (collision not implemented)

--- Game/TileMap.cs
+++ Game/TileMap.cs
@@ -11,11 +11,14 @@ namespace IngredientRun
     {
         TiledMap _map;
         TiledMapRenderer _renderer;
+        TiledMapTileLayer _collision;
 
         public TileMap(string mapPath, ContentManager content, GraphicsDevice graphics)
         {
             _map = content.Load<TiledMap>(mapPath);
             _renderer = new TiledMapRenderer(graphics, _map);
+
+            _collision = _map.GetLayer<TiledMapTileLayer>("Walls");
         }
 
         public void Update(GameTime gameTime)
@@ -28,6 +31,19 @@ namespace IngredientRun
             spriteBatch.Begin(sortMode: SpriteSortMode.Immediate, samplerState: SamplerState.PointClamp);
             _renderer.Draw(viewMatrix, projMatrix);
             spriteBatch.End();
+        }
+
+        public Vector2 GetWaypoint(string layer, string name)
+        {
+            var objects = ((TiledMapObjectLayer)_map.GetLayer(layer)).Objects;
+            for(int i = 0; i < objects.Length; ++i)
+            {
+                if(objects[i].Name == name)
+                {
+                    return objects[i].Position;
+                }
+            }
+            return Vector2.Zero;
         }
     }
 }
