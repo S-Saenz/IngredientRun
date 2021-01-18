141a69acc2fe61599e47f6e0665a0238f2c011cb
aborlin <aborlin@users.noreply.github.com>
2021-01-18 00:08:08 -0700

    scenes created
    as soon as we have the next big commit from alec, I can impliment the scenes

---
+++ States/BlueState.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Pong.Controls;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace IngredientRun.States
+{
+    public class BlueState : State
+    {
+        private List<Component> _components;
+
+        public BlueState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
+          : base(game, graphicsDevice, content)
+        {
+            var buttonTexture = _content.Load<Texture2D>("Controls/Button");
+            var buttonFont = _content.Load<SpriteFont>("Fonts/Font");
+
+            var newGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 200),
+                Text = "New Game",
+            };
+
+            newGameButton.Click += NewGameButton_Click;
+
+            var loadGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 250),
+                Text = "Load Game",
+            };
+
+            loadGameButton.Click += LoadGameButton_Click;
+
+            var quitGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 300),
+                Text = "Quit Game",
+            };
+
+            quitGameButton.Click += QuitGameButton_Click;
+
+            _components = new List<Component>()
+      {
+        newGameButton,
+        loadGameButton,
+        quitGameButton,
+      };
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            _graphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+
+            foreach (var component in _components)
+                component.Draw(gameTime, spriteBatch);
+
+            spriteBatch.End();
+        }
+
+        private void LoadGameButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Load Game");
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            game.ChangeState(new RedState(game, _graphicsDevice, _content));
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            // remove sprites if they're not needed
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var component in _components)
+                component.Update(gameTime);
+        }
+
+        private void QuitGameButton_Click(object sender, EventArgs e)
+        {
+            game.Exit();
+        }
+
+        public override void LoadContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ States/State.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IngredientRun.States
+{
+    public abstract class State
+    {
+        #region Fields
+
+        protected ContentManager _content;
+
+        protected GraphicsDevice _graphicsDevice;
+
+        protected Game1 game;
+
+        //need tile map variable
+
+        #endregion
+
+        #region Methods
+        public State(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
+        {
+            this.game = game;
+
+            _graphicsDevice = graphicsDevice;
+
+            _content = content;
+        }
+
+        public abstract void Initialize();
+
+        public abstract void LoadContent();
+
+
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+
+        public abstract void PostUpdate(GameTime gameTime);
+
+        #endregion
+    }
+}
