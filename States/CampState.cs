1f5bcb6c2e28961064f16908021d6740ab0e8d6c
coagulantLegume <alvwolf@ucsc.edu>
2021-01-27 22:28:22 -0800

    added camp state with dialogue system

---
+++ States/CampState.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
+namespace IngredientRun.States
+{
+    class CampState : State
+    {
+        private NPCDialogueSystem _dialogueSystem;
+
+        public CampState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content, SpriteBatch spriteBatch)
+            : base(game, graphicsDevice, content, spriteBatch)
+        {
+            // initialize NPC dialogue content
+            _dialogueSystem = new NPCDialogueSystem("Content/dialogue/NPCDialogue.tsv");
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            game.GraphicsDevice.Clear(Color.LawnGreen);
+        }
+
+        public override void LoadContent()
+        {
+
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+
+        }
+
+        public override void unloadState()
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
