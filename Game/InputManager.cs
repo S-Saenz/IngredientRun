5d373447b2f97dd4710ce2009faa7ea2792e141e
Edmund Liang <eliang5@ucsc.edu>
2021-02-03 20:51:04 -0800

    fixed one problem

--- Game/InputManager.cs
+++ Game/InputManager.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using Keys = Microsoft.Xna.Framework.Input.Keys;
 
-namespace IngredientRun.Game
+namespace IngredientRun
 {
     class InputManager
     {
