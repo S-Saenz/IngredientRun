ebfca15a71912c8b5f10fa0b926950707bd8f95a
aborlin <aborlin@users.noreply.github.com>
2021-02-24 21:44:05 -0700

    working menu

--- Game/States/MenuState.cs
+++ Game/States/MenuState.cs
@@ -9,33 +9,91 @@ namespace WillowWoodRefuge
 {
     class MenuState : State
     {
-        public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content, SpriteBatch spritebatch) : base(game, graphicsDevice, content, spritebatch)
+        private List<Component> _components;
+
+        public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content, SpriteBatch spritebatch)
+            : base(game, graphicsDevice, content, spritebatch)
         {
+            var buttonTexture = _content.Load<Texture2D>("Controls/Button");
+            var buttonFont = FontManager._dialogueFont;
+
+            var newGameButton = new MenuButton(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(game._cameraController._screenDimensions.X/2 - 80, 200),
+                Text = "New Game",
+            };
+
+            newGameButton.Click += NewGameButton_Click;
+
+            var loadGameButton = new MenuButton(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(game._cameraController._screenDimensions.X / 2 - 80, 300),
+                Text = "Load Game",
+            };
+
+            loadGameButton.Click += LoadGameButton_Click;
+
+            var quitGameButton = new MenuButton(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(game._cameraController._screenDimensions.X / 2 - 80, 400),
+                Text = "Quit Game",
+            };
+
+            quitGameButton.Click += QuitGameButton_Click;
+
+            _components = new List<Component>()
+            {
+                newGameButton,
+                loadGameButton,
+                quitGameButton,
+            };
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            throw new NotImplementedException();
+            game.GraphicsDevice.Clear(Color.Bisque);
+            spriteBatch.Begin();
+
+            foreach (var component in _components)
+                component.Draw(gameTime, spriteBatch);
+
+            spriteBatch.End();
         }
 
         public override void LoadContent()
         {
-            throw new NotImplementedException();
+            
         }
 
         public override void PostUpdate(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            // remove sprite of not needed
         }
 
         public override void unloadState()
         {
-            throw new NotImplementedException();
+            
         }
 
         public override void Update(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            foreach (var component in _components)
+                component.Update(gameTime);
+        }
+
+        private void LoadGameButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Load Game");
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            game.ChangeState("CampState");
+        }
+
+        private void QuitGameButton_Click(object sender, EventArgs e)
+        {
+            game.Exit();
         }
     }
 }
