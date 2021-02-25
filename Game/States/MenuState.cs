2a9c8723944c32e27f5cd06b7eaad9c01a0e18ab
aborlin <aborlin@users.noreply.github.com>
2021-02-24 20:32:39 -0700

    lots

---
+++ Game/States/MenuState.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WillowWoodRefuge
+{
+    class MenuState : State
+    {
+        public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content, SpriteBatch spritebatch) : base(game, graphicsDevice, content, spritebatch)
+        {
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LoadContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void unloadState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
