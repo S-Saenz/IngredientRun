c793fd11aa60dd249984e9020635f3d77352068c
coagulantLegume <alvwolf@ucsc.edu>
2021-03-01 17:19:31 -0800

    Light classes added

---
+++ Game/Lighting/AreaLight.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WillowWoodRefuge
+{
+    class AreaLight
+    {
+        Vector2 _loc;
+        float _dist;
+
+        public AreaLight(Vector2 loc, float dist)
+        {
+            _loc = loc;
+            _dist = dist;
+        }
+    }
+}
---
+++ Game/Lighting/DirectionalLight.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WillowWoodRefuge
+{
+    class DirectionalLight : AreaLight
+    {
+        Vector2 _direction;
+        float _spread;
+
+        public DirectionalLight(Vector2 loc, float dist, Vector2 direction, float spread) : base(loc, dist)
+        {
+            _direction = direction;
+            _spread = spread;
+        }
+    }
+}
