d95f307ad9b14d64aa24b0e910ed6671809161ff
aborlin <aborlin@users.noreply.github.com>
2021-04-07 19:43:18 -0700

    Create stateConditions.cs

---
+++ Game/States/stateConditions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WillowWoodRefuge
+{
+    class stateConditions
+    {
+        public List<Condition> _stateConditions = new List<Condition>();
+
+        public stateConditions()
+        {
+
+        }
+    }
+}
