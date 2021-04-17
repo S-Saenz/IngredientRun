2bccfb5e5e1a58d51db1f59f16f98f1d30f6f9d1
aborlin <aborlin@users.noreply.github.com>
2021-04-16 18:05:29 -0700

    condition manager close

--- Game/Characters/NPCDialogue/NPCHelpers.cs
+++ Game/Characters/NPCDialogue/NPCHelpers.cs
@@ -1,15 +1,23 @@
-﻿namespace WillowWoodRefuge
+﻿using System;
+
+namespace WillowWoodRefuge
 {
     delegate void NPCAction();
     public class Condition
     {
         public string _name;
         public bool _flag;
+        public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
 
         public Condition(string name, bool startState)
         {
             _name = name;
             _flag = startState;
         }
+    }
+
+    public class ThresholdReachedEventArgs : EventArgs
+    {
+        public DateTime TimeReached { get; set; }
     }
 }
--- Game/States/stateConditions.cs
+++ Game/States/stateConditions.cs
@@ -14,7 +14,32 @@ namespace WillowWoodRefuge
             conditionList.Add(new Condition("fedMushroomPrior", true));
             conditionList.Add(new Condition("curedPrior", true));
             conditionList.Add(new Condition("isMorning", true));
-            conditionList.Add(new Condition("isRaining", true));
+            conditionList.Add(new Condition("isRaining", false));
+            conditionList[3].ThresholdReached += c_ThresholdReached;
+        }
+        static void c_ThresholdReached(object sender, ThresholdReachedEventArgs e)
+        {
+            Console.WriteLine("The rain started at {1}.", e.TimeReached);
+            Environment.Exit(0);
+        }
+
+        delegate void NPCAction();
+        public class Condition
+        {
+            public string _name;
+            public bool _flag;
+            public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
+
+            public Condition(string name, bool startState)
+            {
+                _name = name;
+                _flag = startState;
+            }
+        }
+
+        public class ThresholdReachedEventArgs : EventArgs
+        {
+            public DateTime TimeReached { get; set; }
         }
     }
 }
