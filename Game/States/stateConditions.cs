7d7d8966c5c1d247a73ddda8ca3fe1e530b56b88
aborlin <aborlin@users.noreply.github.com>
2021-04-21 12:50:23 -0700

    neccessary changes

--- Game/States/stateConditions.cs
+++ Game/States/stateConditions.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace WillowWoodRefuge
@@ -7,37 +10,63 @@ namespace WillowWoodRefuge
     public class StateConditions
     {
         public List<Condition> conditionList = new List<Condition>();
-
+        delegate void NPCAction();
 
         public StateConditions()
         {
+            // add all the game's onditions
             conditionList.Add(new Condition("fedMushroomPrior", true));
             conditionList.Add(new Condition("curedPrior", true));
             conditionList.Add(new Condition("isMorning", true));
             conditionList.Add(new Condition("isRaining", false));
-            conditionList[3].ThresholdReached += c_ThresholdReached;
+            // add the events that will ocurr on each event
+            conditionList[0].ThisEvent += c_FedMushroom;
         }
-        static void c_ThresholdReached(object sender, ThresholdReachedEventArgs e)
+        // create the event methods
+        static void c_FedMushroom(object sender, OnEventArgs e)
         {
-            Console.WriteLine("The rain started at {1}.", e.TimeReached);
-            Environment.Exit(0);
+            
+            Debug.WriteLine("The mushroom was fed at {1}.", e.TimeReached);
         }
 
-        delegate void NPCAction();
         public class Condition
         {
             public string _name;
             public bool _flag;
-            public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
+
 
             public Condition(string name, bool startState)
             {
                 _name = name;
                 _flag = startState;
             }
+
+            public void OnEvent(OnEventArgs e)
+            {
+                EventHandler<OnEventArgs> handler = ThisEvent;
+                e.TimeReached = DateTime.Now;
+                if (handler != null)
+                {
+                    handler(this, e);
+                }
+            }
+
+            public event EventHandler<OnEventArgs> ThisEvent;
         }
 
-        public class ThresholdReachedEventArgs : EventArgs
+        
+
+        public void ConditionUpdate (GameTime gametime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.O))
+            {
+                OnEventArgs args = new OnEventArgs();
+                args.TimeReached = DateTime.Now;
+                conditionList[0].OnEvent(args);
+            }
+        }
+
+        public class OnEventArgs : EventArgs
         {
             public DateTime TimeReached { get; set; }
         }
