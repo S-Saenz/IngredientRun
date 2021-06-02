15145a6b40b7ff6dd92bc6f4b3fae8b12750ef3e
coagulantLegume <alvwolf@ucsc.edu>
2021-06-01 22:13:03 -0700

    Tutorial page updated with new animations

--- Game/States/TutorialState.cs
+++ Game/States/TutorialState.cs
@@ -14,56 +14,44 @@ namespace WillowWoodRefuge
     {
         private List<UIButton> _components;
 
-        private Animation tutorial;
-
-        Texture2D tuttex;
-
-        private string tut;
+        private List<Animation> _tutorialPages = new List<Animation>();
 
         public string _returnTo;
+
+        Color _bgColor = new Color(203, 170, 142);
 
         public TutorialState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content, SpriteBatch spritebatch) : base(game, graphicsDevice, content, spritebatch)
         {
             var buttonTexture = _content.Load<Texture2D>("Controls/ButtonNormal");
             var buttonFont = FontManager._dialogueFont;
 
-            var menuButton = new UIButton(buttonTexture, new Vector2(game.GraphicsDevice.Viewport.Width / 2 - 80, 100), "Return");
-            menuButton.reScale(2f);
+            var menuButton = new UIButton(buttonTexture, new Vector2((game.GraphicsDevice.Viewport.Width - buttonTexture.Width * 3) / 2, 50), "Return to main");
+            menuButton.reScale(3f);
 
             menuButton.Click += menuButton_Click;
 
             _components = new List<UIButton>()
             {
-                menuButton
+                menuButton,
             };
-            tuttex = _content.Load<Texture2D>("Controls/tutorial");
-            tutorial = new Animation(tuttex, 1, 12, 50);
-
-            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("WillowWoodRefuge.Content.dialogue.Tutorial.txt");
-            // grow system from file
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                // throw away first line (headers)
-                string line;
-                while (!reader.EndOfStream)
-                {
-                    line = reader.ReadLine();
-                    tut += line + '\n';
-                }
-            }
+            _tutorialPages.Add(new Animation(_content.Load<Texture2D>("Tutorial/MovementControls-Sheet"), 1, 12, 50));
+            _tutorialPages.Add(new Animation(_content.Load<Texture2D>("Tutorial/Controls"), 1, 12, 50));
+            // tutorial = new Animation(tuttex, 1, 12, 50);
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            game.GraphicsDevice.Clear(Color.Aqua);
+            game.GraphicsDevice.Clear(_bgColor);
 
             spriteBatch.Begin(sortMode: SpriteSortMode.Immediate, samplerState: SamplerState.PointClamp);
 
             _components[0].Draw(spriteBatch);
 
-            tutorial.Draw(spriteBatch, new Vector2 (game.GraphicsDevice.Viewport.Width/2 + 500, game.GraphicsDevice.Viewport.Height / 2), 2f);
+            _tutorialPages[0].Draw(spriteBatch, new Vector2(400, game.GraphicsDevice.Viewport.Height / 2 + 20), Game1.instance._cameraController._screenScale);
+            _tutorialPages[1].Draw(spriteBatch, new Vector2(game.GraphicsDevice.Viewport.Width - 400, game.GraphicsDevice.Viewport.Height / 2 + 20), Game1.instance._cameraController._screenScale);
 
-            spriteBatch.DrawString(FontManager._bigdialogueFont, tut, new Vector2(20, 70), Color.Black);
+            //_tutorialPages[_currPage].Draw(spriteBatch, new Vector2 (game.GraphicsDevice.Viewport.Width / 2, game.GraphicsDevice.Viewport.Height / 2 + 50), 1f);
+
             spriteBatch.End();
         }
 
@@ -82,7 +70,8 @@ namespace WillowWoodRefuge
         public override void Update(GameTime gameTime)
         {
             _components[0].Update(Mouse.GetState());
-            tutorial.Update(gameTime);
+            foreach(Animation obj in _tutorialPages)
+                obj.Update(gameTime);
         }
 
         private void menuButton_Click(object sender, EventArgs e)
